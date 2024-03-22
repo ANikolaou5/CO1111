d4444e9bf32b13cc95de3c2df2b19fd77539c235
Antreas Kyriacou <akyriakou5@uclan.ac.uk>
2024-03-22 12:50:00 +0200

    cookies

---
+++ js/cookie.js
@@ -0,0 +1,27 @@
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "="  + value + ";" + expires + ";path=/; SameSite=true; Secure=true;";
+    var cookie =document.cookie;
+    console.log(name+value);
+}
+
+function getCookie(name) {
+    let cookie_name = name + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cookie_name) == 0) {
+            return c.substring(cookie_name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+}
